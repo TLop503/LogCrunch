9a786a36c5205884ace1ee360404f2d096ece710
Troy Lopez <troy.n.lopez@gmail.com>
2025-07-28 20:06:26 -0700

    Add tests for log file (depot) parser

--- server/filehandler/log_file_parser.go
+++ server/filehandler/log_file_parser.go
@@ -1,18 +1,29 @@
 package filehandler
 
 import (
-	"github.com/TLop503/LogCrunch/structs"
+	"log"
 	"os"
+
+	"github.com/TLop503/LogCrunch/structs"
 )
 
 // Path to where
-const LOG_INTAKE_DESTINATION = "/var/log/LogCrunch.log"
+const LOG_INTAKE_DESTINATION = "/var/log/LogCrunch/firehose.log"
 
-func LogFileToData() (structs.IntakeLogFileData, error) {
-	content, err := os.ReadFile(LOG_INTAKE_DESTINATION)
-	if err != nil {
-		return structs.IntakeLogFileData{}, err
+func LogFileToData() (*structs.IntakeLogFileData, error) {
+
+	// Check if file exists first
+	if _, err := os.Stat(LOG_INTAKE_DESTINATION); os.IsNotExist(err) {
+		log.Printf("DEBUG: Log depot file does not exist: %s", LOG_INTAKE_DESTINATION)
+		return &structs.IntakeLogFileData{FileContent: "Log file does not exist yet. No logs have been received."}, nil
 	}
 
-	return structs.IntakeLogFileData{FileContent: string(content)}, nil
+	content, err := os.ReadFile(LOG_INTAKE_DESTINATION)
+	if err != nil {
+		log.Printf("DEBUG: Error reading log depot file: %v", err)
+		return &structs.IntakeLogFileData{}, err
+	}
+
+	log.Printf("DEBUG: Successfully read log depot file, content length: %d bytes", len(content))
+	return &structs.IntakeLogFileData{FileContent: string(content)}, nil
 }
